75bf5b37fe097f34fc38562afcfb20b0d2365885
benjamenb <benjamen.brescka@gmail.com>
2014-02-12 00:01:15 -0600

    Display picture by voice finished

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,34 @@
 $(document).ready(initialize);
 
-function intialize() {
-
+function initialize() {
+  if (annyang) {
+    annyang.debug()
+    console.log('it started')
+    annyang.addCommands(commands);
+    annyang.start();
+  } 
 }
 
 function searchFlickr(query) {
   var apiKey = "a050beda319aa138dfd0f6b9724c8155"
   var perPage = 1
   var page = 1
-  var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&text=' + query + '&per_page=' + PER_PAGE + '&page=' + page + '&format=json&jsoncallback=?';
-  $getJSON(url, displayImage)
+  var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&text=' + query + '&per_page=' + perPage + '&page=' + page + '&format=json&jsoncallback=?';
+  $.getJSON(url, displayImage)
 }
 
 function displayImage(data) {
-  photo = data.photos.photo[1]
+  console.log(data)
+  photo = data.photos.photo[0]
   var url = 'url(http://farm'+ photo.farm +'.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
   $('.image-div p').remove()
-  $('image-div').css('background', url)
+  $('.image-div').css({
+    'background': url,
+    'background-repeat' : 'no-repeat',
+    'background-position': 'center'
+  });
+}
+
+var commands = {
+  'I want *desires': searchFlickr
 }
